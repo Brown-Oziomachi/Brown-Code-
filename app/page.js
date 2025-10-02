a47983f86aaff73fe0c37a890282db93d433ab9b
Brown Oziomachi <browncemmanuel@gmail.com>
2025-10-02 16:25:18 +0100

    Add Impact site verification meta tag

--- app/page.js
+++ app/page.js
@@ -1,13 +1,22 @@
 // App.js - Entry Point
 import Portfolio from "@/components/Portfolio";
 import React from "react";
+import Head from "next/head"; // import Head
 
 export default function App() {
-
   return (
-  <div>
-    <Portfolio />
-  </div>
-  )
-}
+    <>
+      <Head>
+        <title>BrownCode Portfolio</title>
+        <meta
+          name="impact-site-verification"
+          value="e9768684-9c09-4886-b2db-ec34003c5283"
+        />
+      </Head>
 
+      <div>
+        <Portfolio />
+      </div>
+    </>
+  );
+}
