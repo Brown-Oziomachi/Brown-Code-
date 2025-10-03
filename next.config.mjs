50824f255bbc034fc87819feb0c55dd0dead9844
Brown Oziomachi <browncemmanuel@gmail.com>
2025-10-03 19:02:34 +0100

    site map added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',   // the URL users and Google see
+        destination: '/api/sitemap', // your API route
+      },
+    ];
+  },
+};
 
 export default nextConfig;
