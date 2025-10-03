795bc45475bd9a325c9adf9b326bc83f8119c152
Brown Oziomachi <browncemmanuel@gmail.com>
2025-10-03 18:34:39 +0100

    site map added

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.browncode.name.ng',
+  generateRobotsTxt: true, // creates robots.txt automatically
+  changefreq: 'daily',
+  priority: 0.7,
+};
