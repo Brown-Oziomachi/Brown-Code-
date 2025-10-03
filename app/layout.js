b3ac621cffe57e92c10eb18c3b3dee3c061a33a1
Brown Oziomachi <browncemmanuel@gmail.com>
2025-10-03 01:59:29 +0100

    advert updated

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,17 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 export const metadata = {
-  title: "Brown code | Sofware Developer",
+  title: "Brown code | Software Developer",
   icons: {
     icon: "/logo.png",
     shortcut: "/logo.png",
     apple: "/logo.png",
+  },
+  verification: {
+    other: {
+      "impact-site-verification": "e9768684-9c09-4886-b2db-ec34003c5283",
+      monetag: "f078e1ec3e8e3ad7a3b1fde84ea75d8e",
+    },
   },
 };
 
@@ -19,7 +25,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-  
 
 export default function RootLayout({ children }) {
   return (
--- app/page.js
+++ app/page.js
@@ -1,21 +1,10 @@
 // App.js - Entry Point
 import Portfolio from "@/components/Portfolio";
 import React from "react";
-import Head from "next/head";
 
 export default function App() {
   return (
     <>
-      <Head>
-        <title>BrownCode Portfolio</title>
-        <meta
-          name="impact-site-verification"
-          content="e9768684-9c09-4886-b2db-ec34003c5283"
-        />
-        {/* Monetag Verification */}
-        <meta name="monetag" content="f078e1ec3e8e3ad7a3b1fde84ea75d8e" />
-      </Head>
-
       <div>
         <Portfolio />
       </div>
