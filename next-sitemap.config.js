8095732a16f5c1684d47293070fb534ff7a6deba
Brown Oziomachi <browncemmanuel@gmail.com>
2026-06-24 12:40:47 +0100

    updated site

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,30 +1,36 @@
 // next-sitemap.config.js
-import { articles } from "./app/data/article";
+import { articles } from "./app/data/article.js";
 
 /** @type {import('next-sitemap').IConfig} */
-export const siteUrl = "https://browncode.name.ng";
-export const generateRobotsTxt = true;
-export const changefreq = "weekly";
-export const priority = 0.7;
-export const exclude = [
-  "/admin_sys_node",
-  "/admin_sys_node/*",
-  "/signin",
-  "/signup",
-  "/cv/pdf",
-  "/floatingchat",
-  "/robots.txt",
-  "/sitemap",
-  "/sitemap-html",
-  "/status",
-  "/bc_dev",
-  "/client/scam-checker",
-];
-export async function additionalPaths() {
-  return articles.map((article) => ({
-    loc: `/blog/${article.slug}`,
-    lastmod: new Date().toISOString(),
-    changefreq: "monthly",
-    priority: 0.9,
-  }));
-}
+const config = {
+  siteUrl: "https://browncode.name.ng",
+  generateRobotsTxt: true,
+  changefreq: "weekly",
+  priority: 0.7,
+
+  exclude: [
+    "/admin_sys_node",
+    "/admin_sys_node/*",
+    "/signin",
+    "/signup",
+    "/cv/pdf",
+    "/floatingchat",
+    "/robots.txt",
+    "/sitemap",
+    "/sitemap-html",
+    "/status",
+    "/bc_dev",
+    "/client/scam-checker",
+  ],
+
+  additionalPaths: async () => {
+    return articles.map((article) => ({
+      loc: `/blog/${article.slug}`,
+      lastmod: new Date().toISOString(),
+      changefreq: "monthly",
+      priority: 0.9,
+    }));
+  },
+};
+
+export default config;
