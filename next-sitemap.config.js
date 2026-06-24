26dd42454437a9bba609011cac23f7758bf6651e
Brown Oziomachi <browncemmanuel@gmail.com>
2026-06-24 12:24:55 +0100

    updated site

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,31 @@
 // next-sitemap.config.js
-module.exports = {
-  siteUrl: 'https://browncode.name.ng',
-  generateRobotsTxt: true,
-}
+import { articles } from "./src/app/data/article";
+// ☝️ adjust this path if your article.js is somewhere else
 
+/** @type {import('next-sitemap').IConfig} */
+export const siteUrl = "https://browncode.name.ng";
+export const generateRobotsTxt = true;
+export const changefreq = "weekly";
+export const priority = 0.7;
+export const exclude = [
+  "/admin_sys_node",
+  "/admin_sys_node/*",
+  "/signin",
+  "/signup",
+  "/cv/pdf",
+  "/floatingchat",
+  "/robots.txt",
+  "/sitemap",
+  "/sitemap-html",
+  "/status",
+  "/bc_dev",
+  "/client/scam-checker",
+];
+export async function additionalPaths() {
+  return articles.map((article) => ({
+    loc: `/blog/${article.slug}`,
+    lastmod: new Date().toISOString(),
+    changefreq: "monthly",
+    priority: 0.9,
+  }));
+}
